d54fe88181301a212c0ded85bed938bfad8fb2fe
Bartosz Czechowski <czechowski.bartek123@gmail.com>
2024-11-17 13:51:09 +0100

    initial files

---
+++ src/cp2024/solution/ParallelCircuitSolver.java
@@ -0,0 +1,19 @@
+package cp2024.solution;
+
+import cp2024.circuit.CircuitSolver;
+import cp2024.circuit.CircuitValue;
+import cp2024.circuit.Circuit;
+
+public class ParallelCircuitSolver implements CircuitSolver {
+    @Override
+    public CircuitValue solve(Circuit c) {
+        /* FIX ME */
+        throw new RuntimeException("Not implemented.");
+    }
+
+    @Override
+    public void stop() {
+        /*FIX ME*/
+        throw new RuntimeException("Not implemented.");
+    }
+}
---
+++ src/cp2024/solution/ParallelCircuitValue.java
@@ -0,0 +1,11 @@
+package cp2024.solution;
+
+import cp2024.circuit.CircuitValue;
+
+public class ParallelCircuitValue implements CircuitValue {
+    @Override
+    public boolean getValue() throws InterruptedException {
+        /* FIX ME */
+        throw new RuntimeException("Not implemented.");
+    }
+}
